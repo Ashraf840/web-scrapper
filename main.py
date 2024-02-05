03f0107da6cc2a4d2fbb62763b36da4e3d0e9918
Ashraf840 <shift2unleashed20112gmail.com>
2024-02-05 23:49:05 +0600

    Modify for production deployment using uwsgi & gunicorn server

--- main.py
+++ main.py
@@ -33,4 +33,4 @@ def website_with_offer():
 # print(website_with_offer(website_list))
 
 if __name__ == '__main__': 
-    app.run(debug = True, port=8080)
+    app.run(debug = False, host="0.0.0.0", port=8080)
